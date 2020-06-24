1c42bd591467106ed184be99d4676ea23508adfa
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 12:18:34 +0300

    changes

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,19 @@
+require 'pry'
+
 def stock_picker(array)
   max_profit = 0
   days = []
   array.each_with_index do |day, index|
-    return 0 unless day.is_a Number
+    return 0 unless day.is_a? Numeric
     array.drop(index).each_with_index do |second_day, second_index|
       profit = second_day - day
       if profit > max_profit
         max_profit = profit
-        days = [day, second_day]
+        days = [index, second_index+1]
       end
     end
   end
   days
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
